796168ec2bbf02fb7065f3ddbec8125c68a81b8d
Stephen Oloto <steevyn51@gmail.com>
2021-06-20 21:57:39 +0100

    added jwt authentication

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,11 +11,16 @@ import {
  } from 'reactstrap';
  import { connect } from 'react-redux';
  import { addItem } from '../actions/itemActions';
+ import PropTypes from 'prop-types'
 
  class ItemModal extends Component {
     state = {
         modal: false,
         name: ''
+    };
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool
     }
 
     toggle = () => {
@@ -45,10 +50,13 @@ import {
     render() {
         return(
             <div>
+
+                { this.props.isAuthenticated ? 
                 <Button
                 color = "dark" style={{marginBottom:'2rem'}}
                 onClick={this.toggle}
-                >Add Item</Button>
+                >Add Item</Button> : <h4 className="mb-3 ml-4">Please log in to manage items</h4>}
+
 
                 <Modal
                 isOpen={this.state.modal}
@@ -77,7 +85,8 @@ import {
  }
 
  const mapStateToProps = (state) => ({
-     item : state.item
+     item : state.item,
+     isAuthenticated: state.auth.isAuthenticated
  });
 
  export default connect(mapStateToProps, { addItem })(ItemModal);
